a1a1c1d5c5b9b071b2ecd796b8cd3dbf32ee89ce
Josh Justice <josh.justice@accesscfa.com>
2020-02-10 12:15:08 -0500

    Fix up test

--- template/src/__tests__/App.spec.js
+++ template/src/__tests__/App.spec.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../App';
 
 describe('App', () => {
   it('renders a welcome message', () => {
-    const {queryByText} = render(<App />);
+    const { queryByText } = render(<App />);
 
-    expect(queryByText('Hello, world!')).not.toBeNull();
+    expect(queryByText('Hello, world.')).not.toBeNull();
   });
 });
