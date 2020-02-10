9a498e43926fb9238600ff6fbeba072c6d1dcb4d
Josh Justice <josh.justice@accesscfa.com>
2020-02-10 12:15:01 -0500

    Function app component

--- template/src/App.js
+++ template/src/App.js
@@ -1,13 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello, world.
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div>Hello, world.</div>;
+};
 
 export default App;
