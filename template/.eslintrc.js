1cc9da34d84b00cfea87ccd90f1962977b2b0db6
Josh Justice <josh.justice@accesscfa.com>
2020-02-10 10:57:53 -0500

    Add cypress and jest to eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
-  extends: ['plugin:react/recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['prettier', 'jest', 'cypress'],
   parser: 'babel-eslint',
   env: {
     browser: true,
+    'cypress/globals': true,
+    es6: true, // for Promise
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   rules: {
     'prettier/prettier': 'error',
