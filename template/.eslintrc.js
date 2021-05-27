26f5019839542a1aee2fc721d92da17d259281a9
Josh Justice <me@codingitwrong.com>
2021-05-27 07:49:23 -0400

    Add import rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
     },
   },
   rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
     'prettier/prettier': 'warn',
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
   },
 };
